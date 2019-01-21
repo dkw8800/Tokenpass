4f59c3a82b9acaa82e9f0d873bfa67f81353f79f
dwong0023 <dwong0023@bths.edu>
2019-01-21 02:24:03 -0500

    ct

--- src/com/company/TokenPass.java
+++ src/com/company/TokenPass.java
@@ -3,6 +3,7 @@ package com.company;
 public class TokenPass {
     int pcount;
     int[] tokens;
+    int crtplayer;
     public TokenPass(int playercount) {
         this.pcount = playercount;
     }
@@ -19,10 +20,15 @@ public class TokenPass {
     }
 
     public void printBoard() {
-
+        for(int i = 0; i < pcount; i++)
+        {
+            System.out.println("Player " + i + ": " + tokens[i] + " ");
+        }
+        System.out.println("Current Player: " + crtplayer);
     }
 
-    public void distributeCurrentPlayerTokens() {
+    public void distributeCurrentPlayerTokens()
+    {
 
     }
 
