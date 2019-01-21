47ad5336194e7b0894f9da96de2c6dd1d83df8a1
dwong0023 <dwong0023@bths.edu>
2019-01-21 02:15:07 -0500

    Initial commit

---
+++ src/com/company/TokenPass.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class TokenPass {
+    int pcount;
+    int[] tokens;
+    public TokenPass(int playercount) {
+        this.pcount = playercount;
+    }
+
+    public boolean gameOver() {
+        for(int i = 0; i < pcount; i++)
+        {
+            if(tokens[i] == 0)
+            {
+                
+            }
+        }
+    }
+
+    public void printBoard() {
+
+    }
+
+    public void distributeCurrentPlayerTokens() {
+
+    }
+
+    public void nextPlayer() {
+
+    }
+}
